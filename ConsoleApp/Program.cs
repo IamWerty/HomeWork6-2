b36e71acaf1dda454f8dc387122f81ff8a3d8aaa
Григорій . <mcwertyge@gmail.com>
2025-06-16 22:26:09 +0300

    add features

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,21 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//  {Console.Write("");
-//  {[var name = Console.ReadLine();
-//  {[{значення2} name = Convert.To{значення2}(Console.ReadLine())
-//Console.WriteLine($"{name}");     $ - Форматування, @ - Сприймати тільки за текст, \n - Перехід на нову строку(писати в "")
-//
-//  Синтаксиз умови:
-//
-//  if (условие)
-//  {
-        // "==" - Дорівнює, "!=" - Не дорівнює, "<"/">" - Меньше/Більше, "<="/">=" - Меньше дорівнює/Більше дорівнює
-//  }
-//  else if (другое_условие)
-//  {
-        // "++" - Збільшення на 1, "--" - Зменьшення на 1, "||" - Або, "&&" - І
-//  }
-//  else
-//  {
-        // 
-//  }
+﻿Console.Write("Вітаю в зірковій прямокутній піраміді!\n Скільки поверхів ви хочете збудувати?\n");
+int numberOfStars = Convert.ToInt16(Console.ReadLine());
+string tower = "*";
+for (int i = 0; i != numberOfStars; i++)
+{
+        Console.WriteLine(tower);
+        tower += "*";
+}
