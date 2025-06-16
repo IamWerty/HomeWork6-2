284555b1791e7bb1ca4278745f725727a095d259
Григорій . <mcwertyge@gmail.com>
2025-06-16 22:27:28 +0300

    add correct encoding

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Вітаю в зірковій прямокутній піраміді!\n Скільки поверхів ви хочете збудувати?\n");
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Вітаю в зірковій прямокутній піраміді!\n Скільки поверхів ви хочете збудувати?\n");
 int numberOfStars = Convert.ToInt16(Console.ReadLine());
 string tower = "*";
 for (int i = 0; i != numberOfStars; i++)
